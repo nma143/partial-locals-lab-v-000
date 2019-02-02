8d47da1c3e3a0720549de22bbb1d0194f149a0b6
Nikki A <nikki.apostolakis@gmail.com>
2019-02-02 02:33:17 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,14 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(name)
+    if name.present?
+      binding.pry
+      where('NAME like ?', "%#{query}%")
+    else
+
+      self.all
+    end
+  end
 end
